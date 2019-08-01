202e2a03f1d683da7c1e2bdbefd0ca15157aa326
AndroidDeveloperLB <lblb636@gmail.com>
2019-08-01 17:45:33 +0300

    set min sdk to 14

--- app/src/main/java/com/sample/myapplication/MainActivity.kt
+++ app/src/main/java/com/sample/myapplication/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.sample.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.ListPopupWindow
@@ -51,5 +51,27 @@ class MainActivity : AppCompatActivity() {
                 holder.itemView.findViewById<TextView>(android.R.id.text1).text = "item $position"
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var url: String? = null
+        when (item.itemId) {
+            R.id.menuItem_all_my_apps -> url = "https://play.google.com/store/apps/developer?id=AndroidDeveloperLB"
+            R.id.menuItem_all_my_repositories -> url = "https://github.com/AndroidDeveloperLB"
+            R.id.menuItem_current_repository_website -> url = "https://github.com/AndroidDeveloperLB/customized-popup-window-sample"
+        }
+        if (url == null)
+            return true
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        @Suppress("DEPRECATION")
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+        startActivity(intent)
+        return true
     }
 }
